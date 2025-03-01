622a4cfd59ae037a2baf516447de8acc88b55287
Antonio <antonioangosoo@gmail.com>
2025-03-01 18:40:49 +0100

    Primer commit - Subida inicial del proyecto

---
+++ listasRepasoGit/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  listasRepasoGit
+//
+//  Created by Toni on 1/3/25.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
